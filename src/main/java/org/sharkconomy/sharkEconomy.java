5f0175df2fa173a8d2cc9c438700b1f0f3139f0a
sharkk2 <129411155+sharkk2@users.noreply.github.com>
2025-01-08 12:30:59 +0200

    Update sharkEconomy.java

--- src/main/java/org/sharkconomy/sharkEconomy.java
+++ src/main/java/org/sharkconomy/sharkEconomy.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.sharkconomy.commands.economy.EconomyCommand;
 import org.sharkconomy.listeners.playerJoin;
 import org.sharkconomy.commands.EconomyTabCompleter;
+import org.sharkconomy.commands.economy.subcommands.Buy;
 
 public final class sharkEconomy extends JavaPlugin {
     private static sharkEconomy instance;
@@ -11,10 +12,11 @@ public final class sharkEconomy extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
-        getLogger().info("HELLO FROM SHARKCONOMY");
+        getLogger().info("HELLO FROM SHARKONOMY");
         getCommand("economy").setExecutor(new EconomyCommand());
         getCommand("economy").setTabCompleter(new EconomyTabCompleter());
         getServer().getPluginManager().registerEvents(new playerJoin(), this);
+        getServer().getPluginManager().registerEvents(new Buy(), this);
     }
 
     @Override
