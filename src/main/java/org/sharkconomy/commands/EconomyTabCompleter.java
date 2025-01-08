799d2f7e702db0fc2e0328243c29f0c5efb6e715
sharkk2 <129411155+sharkk2@users.noreply.github.com>
2025-01-08 12:27:15 +0200

    Update EconomyTabCompleter.java

--- src/main/java/org/sharkconomy/commands/EconomyTabCompleter.java
+++ src/main/java/org/sharkconomy/commands/EconomyTabCompleter.java
@@ -3,6 +3,9 @@ package org.sharkconomy.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -26,6 +29,8 @@ public class EconomyTabCompleter implements TabCompleter {
             suggestions.add("leaderboard");
             suggestions.add("withdraw");
             suggestions.add("deposit");
+            suggestions.add("sell");
+            suggestions.add("buy");
         }
 
         else if (args.length == 2 && "pay".equalsIgnoreCase(args[0])) {
@@ -33,6 +38,33 @@ public class EconomyTabCompleter implements TabCompleter {
                 suggestions.add(player.getName());
             }
         }
+
+        else if (args.length == 2 && "sell".equalsIgnoreCase(args[0])) {
+            Player player = (Player) sender;
+            Inventory inventory = player.getInventory();
+
+            for (ItemStack item : inventory.getContents()) {
+                if (item != null && item.getType() != Material.AIR) {
+                    suggestions.add(item.getType().name());
+                }
+            }
+        }
+
+        else if (args.length == 3 && "sell".equalsIgnoreCase(args[0])) {
+            suggestions.add("1");
+            suggestions.add("2");
+            suggestions.add("5");
+            suggestions.add("10");
+        }
+
+
+        else if (args.length == 4 && "sell".equalsIgnoreCase(args[0])) {
+            suggestions.add("10");
+            suggestions.add("30");
+            suggestions.add("50");
+            suggestions.add("100");
+        }
+
 
         else if (args.length == 2 && "withdraw".equalsIgnoreCase(args[0])) {
             suggestions.add("5");
