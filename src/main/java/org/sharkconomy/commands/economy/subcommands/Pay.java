d945c86931dfc447c8671aea65110785b862039c
sharkk2 <129411155+sharkk2@users.noreply.github.com>
2025-01-08 12:28:37 +0200

    Update Pay.java

--- src/main/java/org/sharkconomy/commands/economy/subcommands/Pay.java
+++ src/main/java/org/sharkconomy/commands/economy/subcommands/Pay.java
@@ -1,6 +1,7 @@
 package org.sharkconomy.commands.economy.subcommands;
 
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.sharkconomy.utils.PlayerData;
@@ -14,44 +15,51 @@ public class Pay {
             return;
         }
 
-
+        double amount;
         try {
-            double amount = Double.parseDouble(args[2]);
+            amount = Double.parseDouble(args[2]);
             if (amount <= 0) {
                 player.sendMessage(ChatColor.RED + "Amount must be larger than 0!");
                 return;
             }
-
-            double balance = PlayerData.getBalance(player.getUniqueId());
-            Player target = Bukkit.getPlayer(args[1]);
-
-            double target_balance = PlayerData.getBalance(target.getUniqueId());
-
-            if (player.getUniqueId().equals(target.getUniqueId())) {
-                player.sendMessage(ChatColor.RED + "You can't pay yourself!");
-                return;
-            }
-
-            if (balance < amount) {
-                player.sendMessage(ChatColor.RED + "You don't have enough sharkcoins!");
-                return;
-            }
-            balance -= amount;
-            target_balance += amount;
-
-            PlayerData.setBalance(player.getUniqueId(), balance);
-            PlayerData.setBalance(target.getUniqueId(), target_balance);
-
-            player.sendMessage("You've transferred §6§l" + amount + "sc§f to§k§2 " + target.getName());
-
-            target.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
-            target.sendMessage("§2§l" + player.getName() + "§f§r has transferred you §6§l" + amount +"sc§r, say thanks!");
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            player.sendMessage(ChatColor.RED + "Amount must be a number!");
+        } catch (NumberFormatException e) {
+            player.sendMessage(ChatColor.RED + "Amount must be a valid number!");
+            return;
         }
 
+        double balance = PlayerData.getBalance(player.getUniqueId());
+
+        OfflinePlayer target = Bukkit.getPlayer(args[1]) != null
+                ? Bukkit.getPlayer(args[1])
+                : Bukkit.getOfflinePlayer(args[1]);
+
+        if (target == null || !target.hasPlayedBefore()) {
+            player.sendMessage(ChatColor.RED + "Couldn't find player: " + args[1]);
+            return;
+        }
+
+        if (player.getUniqueId().equals(target.getUniqueId())) {
+            player.sendMessage(ChatColor.RED + "You can't pay yourself!");
+            return;
+        }
+
+        if (balance < amount) {
+            player.sendMessage(ChatColor.RED + "You don't have enough sharcoins!");
+            return;
+        }
+
+        // Adjust balances
+        double targetBalance = PlayerData.getBalance(target.getUniqueId());
+        PlayerData.setBalance(player.getUniqueId(), balance - amount);
+        PlayerData.setBalance(target.getUniqueId(), targetBalance + amount);
+
+        player.sendMessage(ChatColor.GREEN + "You've transferred §6§l" + amount + "sc§f to §2" + target.getName());
+
+        // If the player is online, notify and play sound
+        if (target.isOnline() && target.getPlayer() != null) {
+            Player onlineTarget = target.getPlayer();
+            onlineTarget.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
+            onlineTarget.sendMessage(ChatColor.GOLD + player.getName() + " has transferred you §6§l" + amount + "sc§r, say thanks!");
+        }
     }
 }
